acd53d1026a1319853cd64bd1c656e883b711223
William <williampesilva@icloud.com>
2021-05-16 14:11:35 -0300

    attCart update

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,5 +1,6 @@
-import { createContext, ReactNode, useContext, useState } from 'react';
+import { createContext, ReactNode, useContext, useEffect, useRef, useState } from 'react';
 import { toast } from 'react-toastify';
+import Cart from '../pages/Cart';
 import { api } from '../services/api';
 import { Product, Stock } from '../types';
 
@@ -32,6 +33,25 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     return [];
   });
 
+  function attCart(updatedCart: Product[]) {
+    setCart(updatedCart);
+    localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart));
+  }
+
+  // const prevCartRef = useRef<Product[]>();
+
+  // useEffect(() => {
+  //   prevCartRef.current = cart;
+  // })
+
+  // const cartPreviousValue = prevCartRef.current ?? cart;
+
+  // useEffect(() => {
+  //   if (cartPreviousValue !== cart) {
+  //     localStorage.setItem('@RocketShoes:cart', JSON.stringify(cart));
+  //   }
+  // }, [cart, cartPreviousValue]);
+
   const addProduct = async (productId: number) => {
     try {
       const updatedCart = [...cart]; // Referencia o updatedCart ao cart, garantindo a imutabilidade
@@ -59,9 +79,9 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
         }
         updatedCart.push(newProduct);
       }
-
-      setCart(updatedCart);
-      localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart));
+      attCart(updatedCart);
+      //setCart(updatedCart);
+      // localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart));
     } catch {
       toast.error('Erro na adição do produto');
     }
@@ -76,8 +96,9 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
         throw Error();
       } else {
         updatedCart.splice(updatedCart.indexOf(productExists), 1);
-        setCart(updatedCart);
-        localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart));
+        attCart(updatedCart);
+        //setCart(updatedCart);
+        // localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart));
       }
     } catch {
       toast.error('Erro na remoção do produto');
@@ -104,8 +125,9 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
 
       if (productExist) {
         productExist.amount = amount;
-        setCart(updatedCart);
-        localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart));
+        attCart(updatedCart);
+        //setCart(updatedCart);
+        // localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart));
       } else {
         throw Error();
       }
